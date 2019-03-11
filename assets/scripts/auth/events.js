d709deb3af985ec051f639228b7407b8c425c9cc
fritzable <fritzzillad@aol.com>
2019-03-11 17:20:37 -0400

    Add handlers for Sign Up
    Add auth event handlers and ajax requests and ui response for POST

---
+++ assets/scripts/auth/events.js
@@ -0,0 +1,56 @@
+'use strict'
+
+const getFormFields = require(`../../../lib/get-form-fields`)
+
+const api = require('./api')
+const ui = require('./ui')
+
+const onSignUp = function (event) {
+  event.preventDefault()
+  console.log('Ran sign up')
+
+  const data = getFormFields(this)
+  api.signUp(data)
+    .then(ui.signUpSuccess)
+    .catch(ui.signUpFailure)
+}
+
+const onSignIn = function (event) {
+  event.preventDefault()
+  console.log('Ran sign in')
+
+  const data = getFormFields(this)
+  api.signIn(data)
+    .then(ui.signInSuccess)
+    .catch(ui.signInFailure)
+}
+
+const onSignOut = function (event) {
+  event.preventDefault()
+  console.log('Ran sign out')
+
+  api.signOut()
+    .then(ui.signOutSuccess)
+    .catch(ui.signOutFailure)
+}
+
+const onChangePassword = function (event) {
+  event.preventDefault()
+  console.log('Ran change password')
+
+  const data = getFormFields(this)
+  api.changePassword(data)
+    .then(ui.changePasswordSuccess)
+    .catch(ui.changePasswordFailure)
+}
+
+const addHandlers = () => {
+  $('#sign-up').on('submit', onSignUp)
+  $('#sign-in').on('submit', onSignIn)
+  $('#sign-out').on('submit', onSignOut)
+  $('#change-password').on('submit', onChangePassword)
+}
+
+module.exports = {
+  addHandlers
+}
